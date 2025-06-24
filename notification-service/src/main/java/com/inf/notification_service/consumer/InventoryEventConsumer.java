e45146dab8cfe4244a2b4c46a11ef3f01db7d995
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2025-06-23 23:01:38 -0300

    feat/show-notifications-toast

--- notification-service/src/main/java/com/inf/notification_service/consumer/InventoryEventConsumer.java
+++ notification-service/src/main/java/com/inf/notification_service/consumer/InventoryEventConsumer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inf.notification_service.model.InventoryEvent;
 import com.inf.notification_service.service.NotificationService;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,10 +12,12 @@ public class InventoryEventConsumer {
 
   private final ObjectMapper objectMapper;
   private final NotificationService notificationService;
+  private final SimpMessagingTemplate messagingTemplate;
 
-  public InventoryEventConsumer(NotificationService notificationService, ObjectMapper objectMapper) {
+  public InventoryEventConsumer(NotificationService notificationService, ObjectMapper objectMapper, SimpMessagingTemplate messagingTemplate) {
     this.notificationService = notificationService;
     this.objectMapper = objectMapper;
+    this.messagingTemplate = messagingTemplate;
   }
 
   @KafkaListener(topics = "inventory-events", groupId = "notification-group")
@@ -23,8 +26,12 @@ public class InventoryEventConsumer {
       System.out.println("Recebeu mensagem do Kafka: " + message);
       InventoryEvent event = objectMapper.readValue(message, InventoryEvent.class);
       notificationService.notifyUser(event);
+
+      // envia via WebSocket para os clientes conectados
+      messagingTemplate.convertAndSend("/topic/notificacoes", message);
     } catch (Exception e) {
       System.err.println("Erro ao processar evento de inventário: " + e.getMessage());
     }
   }
+
 }
